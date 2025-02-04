38079c2eacc311d24fb1622c21e23ea473afa010
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-02-04 10:47:09 +0200

    add custom user model test module

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,25 @@
+"""
+Module for testing any models within the core app. 
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """
+    Class for testing models. 
+    """
+
+    def test_create_user_with_email_success(self):
+        """
+        Test for successful creation of user with email address. 
+        """
+        email = 'test@example.com'
+        password = 'test@123'
+        user = get_user_model().objects.create_user(
+            email=email, 
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))

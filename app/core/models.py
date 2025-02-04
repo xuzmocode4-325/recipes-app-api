db54d15351fce0e7d7cb5d8d165a84a060959295
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-02-04 12:04:46 +0200

    add create_superuser method

--- app/core/models.py
+++ app/core/models.py
@@ -24,7 +24,15 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-        
+    
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Model for custom definition of system user fields"""

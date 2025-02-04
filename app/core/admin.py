d5087f78eeacde89d1ae5da944c66f6d70faa693
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-02-04 16:54:51 +0200

    register user model on app admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin # noqa
+"""
+Django admin customisation
+"""
 
-# Register your models here.
+from django.contrib import admin 
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+from . models import User
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users"""
+    ordering = ['id']
+    list_display = ['name', 'email']
+
+admin.site.register(User, UserAdmin)

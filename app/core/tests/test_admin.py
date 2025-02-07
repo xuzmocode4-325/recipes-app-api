36516f86b5e95934983d26998dcd77e4bf8f1325
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-02-07 03:12:20 +0200

    add test folder to user app

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -28,13 +28,25 @@ class AdminSiteTests(TestCase):
     def test_users_list(self):
         """Test that users are listed on page."""
         url = reverse('admin:core_user_changelist')
-
         res = self.client.get(url)
-        print(f"Response status code: {res.status_code}")
 
-        # Access the changelist from context data
-        # changelist = res.context_data['cl'].queryset
-        # self.assertIn(self.user, changelist)
+        print(f"Response status code: {res.status_code}")
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        print(f"Response status code: {res.status_code}")
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test the create user page works."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)

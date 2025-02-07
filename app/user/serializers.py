8801e51e076f07c29df8057baf2f065f6a99731d
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-02-07 04:09:20 +0200

    fix extra kwargs key for min length

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -9,13 +9,13 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for the user object"""
-    
+
     class Meta:
         model = get_user_model()
         fields = ['email', 'password', 'name']
         extra_kwargs = {'password': {
             'write_only': True, 
-            'main_length': 5
+            'min_length': 5
         }}
 
     def create(self, validated_data): 

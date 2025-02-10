8f37f6dd53d6b6663805edca9b0be391756ddb00
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-02-10 05:06:18 +0200

    add recipe model test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,10 @@
 """
 Module for testing any models within the core app.
 """
+from decimal import Decimal
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from .. import models
 
 
 class ModelTests(TestCase):
@@ -54,3 +56,23 @@ class ModelTests(TestCase):
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
         print(f"Super user creation test: successful")
+
+
+    def test_create_recipe(self):
+        "Test creating a recipe is successful"
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'passypasspassword123'
+        )
+
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample recipe description'
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
+
+

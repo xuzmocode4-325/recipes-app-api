075c8d2596bc5236eca1ef67b906d17c38a34391
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-02-12 04:56:13 +0200

    add tag api view, router and serializer

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,11 +1,11 @@
 """
 Views for the recipe API
 """
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import Recipe, Tag
 from . import serializers
 
 
@@ -29,3 +29,20 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create new recipe"""
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(
+        mixins.DestroyModelMixin,
+        mixins.UpdateModelMixin,
+        mixins.ListModelMixin,
+        viewsets.GenericViewSet):
+    """Manange tags in the database"""
+
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    autentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')

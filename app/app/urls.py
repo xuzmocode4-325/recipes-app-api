e962065b864230ab4dd879924f12861e58fc8743
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-02-12 04:55:21 +0200

    lint urls.py

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,6 +21,7 @@ from drf_spectacular.views import (
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),

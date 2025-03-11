baa8d6e5790d95dca5ae5f3b0eea632bc9662e6c
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-03-11 03:40:10 +0200

    add development server configs for media files

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,6 +20,8 @@ from drf_spectacular.views import (
 )
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -33,3 +35,9 @@ urlpatterns = [
     path('api/user/', include('user.urls'), name='user'),
     path('api/recipe/', include('recipe.urls'), name='recipe'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, 
+        document_root=settings.MEDIA_ROOT
+    )
